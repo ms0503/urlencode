203e1dbeacdfa8222d9583a29829d5eb84da6587
Sora Tonami <ms0503@outlook.com>
2026-06-09 02:05:47 +0900

    chore: add nix config

--- flake.nix
+++ flake.nix
@@ -29,6 +29,23 @@
       url = "github:numtide/treefmt-nix";
     };
   };
+  nixConfig = {
+    experimental-features = [
+      "flakes"
+      "nix-command"
+      "pipe-operators"
+    ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://ms0503.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "ms0503.cachix.org-1:Cc2mXpepZr7O9aFcRj5jq3mIcvdUPp85sLFuQj+IKbM="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   outputs =
     inputs@{ flake-parts, systems, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
